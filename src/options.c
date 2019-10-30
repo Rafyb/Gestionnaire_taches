b60b97a898199e913d8e2c2cee77e52f58c7f8ba
johann <johann.de-almeida@lacatholille.fr>
2019-10-30 11:24:13 +0100

    for boucle is ok

--- src/options.c
+++ src/options.c
@@ -3,72 +3,73 @@
 #include <unistd.h>
 #include <errno.h>
 
-int option1 = 0;
-int option2 = 0;
-int option3 = 0;
-int option4 = 0;
-int option5 = 0;
+int A = 0;
+int B = 0;
+int C = 0;
+int D = 0;
+int E = 0;
 
 int j = 1;
 
 int main(int argc, char** argv){
-	while (argv[j][0]=='-'){
-		int idx = 1;
-		while (argv[j][idx]!=0){
-			if (argv[j][idx]=='option1')option1=j;
-			if (argv[j][idx]=='option2')option2=j;
-			if (argv[j][idx]=='option3')option3=j;
-			if (argv[j][idx]=='option4')option4=j;
-			if (argv[j][idx]=='option5')option5=j;
-			idx++;
+	for (j; j< argc; j++) {
+		if (argv[j][0]=='-'){
+			int idx = 1;
+			while (argv[j][idx]!='\0'){
+				if (argv[j][idx]=='A')A=j;
+				if (argv[j][idx]=='B')B=j;
+				if (argv[j][idx]=='C')C=j;
+				if (argv[j][idx]=='D')D=j;
+				if (argv[j][idx]=='E')E=j;
+				idx++;
+			}
 		}
-		j++;
 	}
 	
 	char chaine[100];
-		if (option1 != 0) {
-			if (argv[option1 +1][0]=='-') {
+		if (A != 0) {
+			if (argv[A +1][0]=='-') {
 				printf("Le chemin n'a pas été spécifié");
 			}
 			else
 			{
-				printf("option1: %s\n", argv[option1 +1]);
+				printf("A: %s\n", argv[A +1]);
 			}
 		}
-		if (option2 != 0) {
-			if (argv[option2 +1][0]=='-') {
+		if (B != 0) {
+			if (argv[B +1][0]=='-') {
 				printf("Le chemin n'a pas été spécifié");
 			}
 			else
 			{
-				printf("option2: %s\n", argv[option2 +1]);
+				printf("B: %s\n", argv[B +1]);
 			}
 		}
-		if (option3 != 0) {
-			if (argv[option3 +1][0]=='-') {
+		if (C != 0) {
+			if (argv[C +1][0]=='-') {
 				printf("Le chemin n'a pas été spécifié");
 			}
 			else
 			{
-				printf("option3: %s\n", argv[option3 +1]);
+				printf("C: %s\n", argv[C +1]);
 			}
 		}
-		if (option4 != 0) {
-			if (argv[option4 +1][0]=='-') {
+		if (D != 0) {
+			if (argv[D +1][0]=='-') {
 				printf("Le chemin n'a pas été spécifié");
 			}
 			else
 			{
-				printf("option4: %s\n", argv[option4 +1]);
+				printf("D: %s\n", argv[D +1]);
 			}
 		}
-		if (option5 != 0) {
-			if (argv[option5 +1][0]=='-') {
+		if (E != 0) {
+			if (argv[E +1][0]=='-') {
 				printf("Le chemin n'a pas été spécifié");
 			}
 			else
 			{
-				printf("option5: %s\n", argv[option5 +1]);
+				printf("E: %s\n", argv[E +1]);
 			}
 		}	
 	
